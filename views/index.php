655d376e4b4f508953794dd44bbd2a55dfdf985c
Mwelu Mutinda <mutinda.mwelu@ajiraconnect.com>
2020-07-07 14:23:34 +0300

    africastalking php

---
+++ views/index.php
@@ -0,0 +1,11 @@
+<div class="col-md-6">
+            <pre><span id="response"></span></pre>
+        </div>
+
+        <div class="col-md-6">
+            <div class="col-sm-6 col-offset-sm-3">
+                <input id="phone" type="text" class="form-control" placeholder="Your phone number">
+            </div>
+            <p><a class="btn btn-success btn-sm" role="button" id="sendSms">Send SMS</a></p>
+            
+        </div>
